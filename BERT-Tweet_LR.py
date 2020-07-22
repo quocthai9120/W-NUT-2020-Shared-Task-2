834be26fcb503b73f1e67c4daab6c9f20abc7370
vinai <thuphuongvu.amser@gmail.com>
2020-07-22 19:52:36 +0700

    added resNet

--- BERT-Tweet_LR.py
+++ BERT-Tweet_LR.py
@@ -7,12 +7,12 @@ import logging
 import pandas as pd
 import numpy as np
 from numpy import random
-import gensim
+# import gensim
 import nltk
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 import re
 from bs4 import BeautifulSoup
--- BERTweetForBinaryClassification.py
+++ BERTweetForBinaryClassification.py
@@ -17,39 +17,15 @@ class BERTweetForBinaryClassification(BertPreTrainedModel):
             "./BERTweet_base_transformers/model.bin",
             config=config
         )
-<<<<<<< HEAD
 
         # self.classifier = model
 
         self.resnet = models.resnet18(pretrained=True)
         num_final_in = self.resnet.fc.in_features
-        self.classifier = nn.Linear(in_features=num_final_in,
-=======
-        self.dense = nn.Linear(in_features=768,
-                               out_features=64,
-                               )
-        self.dropout = nn.Dropout(p=0.2)
-        self.dense_2 = nn.Linear(in_features=64,
-                                 out_features=64,
-                                 )
-        self.classifier = nn.Linear(in_features=64,
->>>>>>> de55464ffe68e9d5ed43f624eefef2ed53dc2721
+        self.resnet.fc = nn.Linear(in_features=num_final_in,
                                     out_features=self.num_labels,
                                     )
 
-# model = models.resnet18(pretrained = True)
-num_final_in = model.fc.in_features
-NUM_CLASSES = 1
-model.fc = nn.Linear(num_final_in, NUM_CLASSES)
-
-# Code for freezing different layers
-ct = 0
-for child in model.children():
-    ct += 1
-    if ct < 4:
-        for param in child.parameters():
-            param.require_grad = False
-            
     def forward(
         self,
         input_ids=None,
@@ -63,15 +39,7 @@ for child in model.children():
 
         # Take <CLS> token for Native Layer Norm Backward
         sequence_output = outputs[0][:, 0, :]
-<<<<<<< HEAD
-        sequence_output = self.resnet(sequence_output)
-=======
-        sequence_output = self.dense(sequence_output)
-        sequence_output = self.dropout(sequence_output)
-        sequence_output = self.dense_2(sequence_output)
-        sequence_output = self.dropout(sequence_output)
->>>>>>> de55464ffe68e9d5ed43f624eefef2ed53dc2721
-        logits = self.classifier(sequence_output)
+        logits = self.resnet(sequence_output)
         outputs = (logits,)
         if labels is not None:
             loss_function = CrossEntropyLoss()
