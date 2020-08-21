966cbdce7280be0753eccf0fa89812100fe5bb78
Thai Quoc Hoang <quocthai9120@gmail.com>
2020-08-21 16:52:47 +0700

    .

--- last_k_layers_BERTweet_model.py
+++ last_k_layers_BERTweet_model.py
@@ -19,11 +19,11 @@ class BERTweetModelForClassification(BertPreTrainedModel):
             "./BERTweet_base_transformers/model.bin",
             config=config
         )
-        self.dense = nn.Linear(in_features=1536,
-                               out_features=768,
+        self.dense = nn.Linear(in_features=3072,
+                               out_features=1536,
                                )
         self.dropout = nn.Dropout(p=0.2)
-        self.dense_2 = nn.Linear(in_features=768,
+        self.dense_2 = nn.Linear(in_features=1536,
                                  out_features=256,
                                  )
         self.classifier = nn.Linear(in_features=256,
@@ -44,10 +44,14 @@ class BERTweetModelForClassification(BertPreTrainedModel):
         hidden_states: Tuple[torch.tensor] = outputs[2]
         last_sequence_output: torch.tensor = hidden_states[-1][:, 0, :]
         second_to_last_sequence_output: torch.tensor = hidden_states[-2][:, 0, :]
+        third_to_last_sequence_output: torch.tensor = hidden_states[-3][:, 0, :]
+        fourth_to_last_sequence_output: torch.tensor = hidden_states[-4][:, 0, :]
 
         sequence_output: torch.tensor = torch.cat((
             last_sequence_output,
             second_to_last_sequence_output,
+            third_to_last_sequence_output,
+            fourth_to_last_sequence_output
         ), dim=1)
 
         sequence_output = self.dense(sequence_output)
