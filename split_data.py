f0419500a4e389a166bb892fdb47687d5206cfd8
vinai <thuphuongvu.amser@gmail.com>
2020-09-01 23:56:07 +0700

    final final data & split data script

--- split_data.py
+++ split_data.py
@@ -12,21 +12,30 @@ def tweet_length(dataframe):
         word_arr.append(len(line.split()))
     dataframe['Num_words'] = word_arr
 
-def ... classify into 3 classes
+# 3 classes: 0-22, 22-44, 44-max => 0, 1, 2, respectively
+def classify_tweet_length(dataframe):
+    dataframe['Length_class'] = ""
+    dataframe.loc[dataframe['Num_words'] < 22, 'Length_class'] = 0
+    dataframe.loc[(22 <= dataframe['Num_words']) & (dataframe['Num_words'] <= 44), 'Length_class'] = 1
+    dataframe.loc[dataframe['Num_words'] > 44, 'Length_class'] = 2
+
 
 # Read data in
 df_train = pd.read_csv('./data/train.csv')
 df_train = df_train[pd.notnull(df_train['Label'])]
 tweet_length(df_train)
+classify_tweet_length(df_train)
 
 
 df_valid = pd.read_csv('./data/valid.csv')
 df_valid = df_valid[pd.notnull(df_valid['Label'])]
 tweet_length(df_valid)
+classify_tweet_length(df_valid)
 
 df_test = pd.read_csv('./data/test.csv')
 df_test = df_test[pd.notnull(df_test['Label'])]
 tweet_length(df_test)
+classify_tweet_length(df_test)
 
 
 '''
@@ -37,3 +46,5 @@ df_train.to_csv(path_or_buf="./final_final_data/train.csv", index=False)
 df_valid.to_csv(path_or_buf="./final_final_data/valid.csv", index=False)
 
 df_test.to_csv(path_or_buf="./final_final_data/test.csv", index=False)
+
+
